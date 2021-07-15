579de9a1ab33fbf418d36d925a8c4066573806dc
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-07-15 14:29:12 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 import { obtenerClientes, eliminarCliente } from './API.js';
+let tipo = document.querySelector('#tp');
+const formulario = document.querySelector('#formulario');
+formulario.addEventListener('submit', buscarcliente);
 
 (function() {
     const listado = document.querySelector('#listado-clientes');
@@ -50,3 +53,102 @@ import { obtenerClientes, eliminarCliente } from './API.js';
 
 })();
 
+async function buscarcliente()
+{
+    const terminoBusqueda = document.querySelector('#termino').value;
+    listado.innerHTML="";
+    const clientes = await obtenerClientes();
+    if(tipo.value=="")
+    {
+        mostrarClientes();
+    }
+
+        clientes.forEach( cliente => {
+            const { nombre, email, telefono, empresa, id } = cliente;
+            const row = document.createElement('tr');
+
+             
+
+
+             if(tipo.value=="Nombre")
+             {
+
+            if(terminoBusqueda==nombre)
+            {
+                row.innerHTML += `
+                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                    <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                </td>
+                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                    <p class="text-gray-700">${telefono}</p>
+                </td>
+                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                    <p class="text-gray-600">${empresa}</p>
+                </td>
+                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                    <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                    <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
+                </td>
+            `;
+            }else{
+                row.innerHTML += `Sin Resultados`;
+            }
+
+
+             }
+             else if(tipo.value=="Telefono")
+             {
+                if(terminoBusqueda==telefono)
+                {
+                    row.innerHTML += `
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                        <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                        <p class="text-gray-700">${telefono}</p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                        <p class="text-gray-600">${empresa}</p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                        <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                        <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
+                    </td>
+                `;
+                }else{
+                    row.innerHTML += `Sin Resultados`;
+                }
+
+             }else if(tipo.value=="Empresa")
+             {
+                if(terminoBusqueda==empresa)
+                {
+                    row.innerHTML += `
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                        <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                        <p class="text-gray-700">${telefono}</p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                        <p class="text-gray-600">${empresa}</p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                        <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                        <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
+                    </td>
+                `;
+                }else{
+                    row.innerHTML += `Sin Resultados`;
+                }
+             }
+           
+          
+
+            listado.appendChild(row);
+        })
+    
+}
